da8deba269f5b3459ffc00797251f361e44b4262
projects-lab <guillermobedoya104@gmail.com>
2022-09-22 16:45:33 -0500

    inicial

---
+++ resources/views/head.blade.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizito</title>
+    <link rel="stylesheet" href="style.css">
+    @vite('resources/css/app.css')
+    <script src="/app.js"></script>
+</head>
